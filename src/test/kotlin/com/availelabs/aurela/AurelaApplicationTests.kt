8c76ebe8aaa294e189a8282c810ac37d9bb00f03
availe <94802525+availe@users.noreply.github.com>
2026-07-15 12:57:45 -0400

    project init

---
+++ src/test/kotlin/com/availelabs/aurela/AurelaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.availelabs.aurela
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AurelaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
