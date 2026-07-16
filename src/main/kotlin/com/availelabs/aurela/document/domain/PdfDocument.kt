4094b099a197ae11957e197f07f444836f088dad
availe <94802525+availe@users.noreply.github.com>
2026-07-16 12:21:18 -0400

    task: delete code files that are not needed for time being

--- src/main/kotlin/com/availelabs/aurela/document/domain/PdfDocument.kt
+++
@@ -1,9 +0,0 @@
-package com.availelabs.aurela.document.domain
-
-import kotlin.time.Instant
-import kotlin.uuid.Uuid
-
-data class PdfDocument(
-    val id: Uuid,
-    val uploadedAt: Instant
-)
--- src/main/kotlin/com/availelabs/aurela/document/infrastructure/DocumentRepository.kt
+++
@@ -1,4 +0,0 @@
-package com.availelabs.aurela.document.infrastructure
-
-class DocumentRepository {
-}
