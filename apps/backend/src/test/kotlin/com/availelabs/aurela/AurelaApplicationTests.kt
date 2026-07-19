226f81951e455b9859a612b4efd04944f325dc91
availe <94802525+availe@users.noreply.github.com>
2026-07-19 00:59:47 -0400

    fix: provide Postgre service connection for context load in AurelaApplicationTests.kt

--- apps/backend/src/test/kotlin/com/availelabs/aurela/AurelaApplicationTests.kt
+++ apps/backend/src/test/kotlin/com/availelabs/aurela/AurelaApplicationTests.kt
@@ -2,9 +2,20 @@ package com.availelabs.aurela
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.postgresql.PostgreSQLContainer
 
+@Testcontainers
 @SpringBootTest
 class AurelaApplicationTests {
+    companion object {
+        @Container
+        @ServiceConnection
+        @JvmStatic
+        val postgres = PostgreSQLContainer("postgres:19beta2")
+    }
 
     @Test
     fun contextLoads() {
