d9d0d914c9624e4738100a63920823b2a47d4997
availe <94802525+availe@users.noreply.github.com>
2026-07-24 12:24:59 -0400

    task: centralize dependency repositories in settings in settings.gradle.kt

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
 rootProject.name = "aurela"
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
+
 include("apps:backend")
